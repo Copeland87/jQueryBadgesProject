179726c0caf5a9fd79bac4e8d10bb442e2d04041
Zac Copeland <zcopeland87@gmail.com>
2017-10-24 17:00:19 -0500

    ajax

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,4 +2,14 @@ $(function() {
 
   // your code will go here
 
+  $.ajax({
+  	url: 'https://www.codeschool.com/users/Copeland87.json',
+  	dataType: 'jsonp',
+  	success: function(response) {
+  		// handle response
+  	}
+  })
+
+
+
 });
